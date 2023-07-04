f0307d96ecd28ca8d6a6ffbee110d62a3e2b25fc
Mohammadreza <mohammadrezatagipour45@gmail.com>
2023-07-04 18:49:11 +0330

    Add Routing

--- Amazona/frontend/src/App.js
+++ Amazona/frontend/src/App.js
@@ -1,33 +1,24 @@
 import data from './data';
+
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen.jsx';
+import ProductScreen from './screens/ProductScreen';
+// import { HomeScreen } from './screens/HomeScreen';
 function App() {
   return (
-    <div>
-      <header>
-        <a href="/">amazona</a>
-      </header>
-      <main>
-        <h1>Featured Products </h1>
-        <div className="products">
-          {data.products.map((product) => (
-            <div className="product" key={product.slug}>
-              <a href={`/products/${product.slug}`}>
-                <img src={product.image} alt={product.name} />
-              </a>
-              <div className="product-info">
-                <a href={`/products/${product.slug}`}>
-                  <p>{product.name}</p>
-                </a>
-                <p>
-                  {' '}
-                  <strong>${product.price}</strong>{' '}
-                </p>
-                <button>Add to cart</button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />} />
+            <Route path="/products/:slug" element={<ProductScreen />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 

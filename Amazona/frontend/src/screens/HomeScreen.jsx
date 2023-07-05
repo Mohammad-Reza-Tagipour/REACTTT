ab2e422d94a4c9d3d00791dea78cd744242b16b4
Mohammadreza <mohammadrezatagipour45@gmail.com>
2023-07-05 16:56:14 +0330

    Fetch products from server

--- Amazona/frontend/src/screens/HomeScreen.jsx
+++ Amazona/frontend/src/screens/HomeScreen.jsx
@@ -1,13 +1,23 @@
 import { Link } from 'react-router-dom';
-import data from '../data';
-
+// import data from '../data';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 function HomeScreen() {
+  // useState return an array that contains variable(products) and a function to update this vriable
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    const fetchDate = async () => {
+      const result = await axios.get('/api/products');
+      setProducts(result.data);
+    };
+    fetchDate();
+  }, []);
   return (
     <div>
       {' '}
       <h1>Featured Products </h1>
       <div className="products">
-        {data.products.map((product) => (
+        {products.map((product) => (
           <div className="product" key={product.slug}>
             <Link to={`/products/${product.slug}`}>
               <img src={product.image} alt={product.name} />
